ef07acbd382444be6293607ef4a1363b9bd254a3
Alexandre <alexandrefaria3012@gmail.com>
2024-09-17 10:28:59 -0300

    Update Calculadora.jsx

--- src/componentes/Calculadora.jsx
+++ src/componentes/Calculadora.jsx
@@ -1,6 +1,26 @@
 const Calculadora = () => {
     return(
-        <h1>alo</h1>
+        <div>
+            <button>AC</button>
+            <button>+/-</button>
+            <button>%</button>
+            <button>/</button>
+            <button>X</button>
+            <button>-</button>
+            <button>+</button>
+            <button>=</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+            <button>,</button>
+        </div>
     )
 }
 
