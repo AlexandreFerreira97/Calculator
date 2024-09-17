0015e6dee3f917a6f8d25b0da4d8b9006312f7a6
Alexandre <alexandrefaria3012@gmail.com>
2024-09-17 11:25:20 -0300

    Update Calculadora.jsx

--- src/componentes/Calculadora.jsx
+++ src/componentes/Calculadora.jsx
@@ -2,26 +2,31 @@ import React from "./Calculadora.css"
 
 const Calculadora = () => {
     return(
-        <div>
-            <button className="botaoEsp">AC</button>
-            <button className="botaoEsp">+/-</button>
-            <button className="botaoEsp">%</button>
-            <button className="botaoOp">/</button>
-            <button className="botaoOp" >X</button>
-            <button className="botaoOp">-</button>
-            <button className="botaoOp">+</button>
-            <button className="botaoOp">=</button>
-            <button className="botaoNum">7</button>
-            <button className="botaoNum">8</button>
-            <button className="botaoNum">9</button>
-            <button className="botaoNum">4</button>
-            <button className="botaoNum">5</button>
-            <button className="botaoNum">6</button>
-            <button className="botaoNum">1</button>
-            <button className="botaoNum">2</button>
-            <button className="botaoNum">3</button>
-            <button className="botaoNum">0</button>
-            <button className="botaoNum">,</button>
+        <div className="container">
+            <div className="esp">
+                <button className="botaoEsp">AC</button>
+                <button className="botaoEsp">+/-</button>
+                <button className="botaoEsp">%</button>
+                <button className="botaoNum">7</button>
+                <button className="botaoNum">8</button>
+                <button className="botaoNum">9</button>
+                <button className="botaoNum">4</button>
+                <button className="botaoNum">5</button>
+                <button className="botaoNum">6</button>
+                <button className="botaoNum">1</button>
+                <button className="botaoNum">2</button>
+                <button className="botaoNum">3</button>
+                <button className="botaoNum">0</button>
+                <button className="botaoNum">,</button>
+            </div>
+
+            <div className="op"> 
+                <button className="botaoOp">/</button>
+                <button className="botaoOp" >X</button>
+                <button className="botaoOp">-</button>
+                <button className="botaoOp">+</button>
+                <button className="botaoOp">=</button>
+            </div>
         </div>
     )
 }
