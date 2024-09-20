28a7b215c271af76fd81c24f98a875f27396395a
Alexandre <alexandrefaria3012@gmail.com>
2024-09-19 22:45:20 -0300

    Update Calculadora.jsx

--- src/componentes/Calculadora.jsx
+++ src/componentes/Calculadora.jsx
@@ -1,26 +1,35 @@
+import { useState } from "react"
 import React from "./Calculadora.css"
 
 const Calculadora = () => {
+
+    const [numero, setNumero] = useState(0)
+
+    function inputNumero(valor){
+        var num = valor.target.value
+        setNumero(num)
+    }
+
     return(
         <div className="calculadora">
             <div className="container">
                 <div className="resultado">
-                    <h1>rere</h1>
+                    <h1>{numero}</h1>
                 </div>
                 <div className="esp">
                     <button className="botaoEsp">AC</button>
                     <button className="botaoEsp">+/-</button>
                     <button className="botaoEsp">%</button>
-                    <button className="botaoNum">7</button>
-                    <button className="botaoNum">8</button>
-                    <button className="botaoNum">9</button>
-                    <button className="botaoNum">4</button>
-                    <button className="botaoNum">5</button>
-                    <button className="botaoNum">6</button>
-                    <button className="botaoNum">1</button>
-                    <button className="botaoNum">2</button>
-                    <button className="botaoNum">3</button>
-                    <button className="botaoNum ">0</button>
+                    <button className="botaoNum" onClick={inputNumero} value={7}>7</button>
+                    <button className="botaoNum" onClick={inputNumero} value={8}>8</button>
+                    <button className="botaoNum" onClick={inputNumero} value={9}>9</button>
+                    <button className="botaoNum" onClick={inputNumero} value={4}>4</button>
+                    <button className="botaoNum" onClick={inputNumero} value={5}>5</button>
+                    <button className="botaoNum" onClick={inputNumero} value={6}>6</button>
+                    <button className="botaoNum" onClick={inputNumero} value={1}>1</button>
+                    <button className="botaoNum" onClick={inputNumero} value={2}>2</button>
+                    <button className="botaoNum" onClick={inputNumero} value={3}>3</button>
+                    <button className="botaoNum " onClick={inputNumero} value={0}>0</button>
                     <button className="botaoNum">,</button>
                 </div>
 
